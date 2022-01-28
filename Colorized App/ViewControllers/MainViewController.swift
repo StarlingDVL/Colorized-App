f76ec0f5b04a1c7c99e08833de40decf0393b804
ISkvor <starlingy608@gmail.com>
2022-01-28 23:06:34 +0300

    set prepare for segue from main VC

--- Colorized App/ViewControllers/MainViewController.swift
+++ Colorized App/ViewControllers/MainViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func setColor(_ color: UIColor)
+}
+
 class MainViewController: UIViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.mainViewColor = view.backgroundColor
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
     }
 }
--- Colorized App/ViewControllers/SettingsViewController.swift
+++ Colorized App/ViewControllers/SettingsViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    // MARK: - IB Outlets
 
     @IBOutlet var colorView: UIView!
     
@@ -25,34 +27,98 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet var doneButton: UIButton!
     
+    // MARK: - Public properties
+    var mainViewColor: UIColor!
+    var delegate: SettingsViewControllerDelegate!
+    
+    // MARK: - Override functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        doneButton.layer.cornerRadius = 10
         
-        changeColor()
-        changeLabelText()
+        doneButton.layer.cornerRadius = 15
+        
+        colorView.layer.cornerRadius = 15
+        colorView.backgroundColor = mainViewColor
+        
+        setStartSliderValues()
+        
+        changeText(for: redValueLabel, greenValueLabel, blueValueLabel)
+        changeText(for: redTextField, greenTextField, blueTextField)
+        
+    
     }
+    
+    // MARK: - IB Actions
 
     @IBAction func rgbSliderAction(_ sender: UISlider) {
         changeColor()
         
-        changeLabelText()
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+            redTextField.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+            greenTextField.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+            blueTextField.text = string(from: blueSlider)
+        }
     }
 
     @IBAction func doneButtonPressed() {
     }
     
-    private func changeColor() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
-                                            green: CGFloat(greenSlider.value),
-                                            blue: CGFloat(blueSlider.value),
-                                            alpha: 1)
+    
+    // MARK: - Private methods
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
     
-    private func changeLabelText() {
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
+    
+    private func changeText(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                redValueLabel.text = string(from: redSlider)
+            case greenValueLabel:
+                greenValueLabel.text = string(from: greenSlider)
+            default:
+                blueValueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func changeText(for textFields: UITextField...) {
+        textFields.forEach { textField in
+            switch textField {
+            case redTextField:
+                redTextField.text = string(from: redSlider)
+            case greenTextField:
+                greenTextField.text = string(from: greenSlider)
+            default:
+                blueTextField.text = string(from: blueSlider)
+            }
+        }
+    }
+    
+    private func setStartSliderValues() {
+        let rgbForSliders = CIColor(color: mainViewColor)
+        
+        redSlider.value = Float(rgbForSliders.red)
+        greenSlider.value = Float(rgbForSliders.green)
+        blueSlider.value = Float(rgbForSliders.blue)
+    }
+    
 }
 
